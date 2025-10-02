a6ead671ad71e5d605d50705d3e29c274f8bc80a
chandrasekar <chandrasekarms11@gmail.com>
2025-10-02 19:04:08 +0530

    adding ml_model.py adding three models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("Adding ML related code here")
+
+def model1():
+    print("adding code to model1 algorithm")
+
+def model2():
+    print("adding code to model2 algorithm")
+
+def model3():
+    print("adding code to model3 algorithm")
